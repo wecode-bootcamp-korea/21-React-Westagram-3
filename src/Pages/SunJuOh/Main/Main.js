3630e55fe5b16a232d907572a1a2d6f447c32397
Jay Park <devpark.0x1c@gmail.com>
2021-05-27 11:30:44 +0900

    Fix: Prettier 자동 수정

--- src/Pages/SunJuOh/Main/Main.js
+++ src/Pages/SunJuOh/Main/Main.js
@@ -1,11 +1,9 @@
-import React from 'react'
-import "./Main.scss"
+import React from 'react';
+import './Main.scss';
 
 class Main extends React.Component {
   render() {
-    return (
-      <div>test</div>
-    )
+    return <div>test</div>;
   }
 }
 
