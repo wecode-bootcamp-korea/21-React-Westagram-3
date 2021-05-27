3e56677679b73c2681180351281263c77b80ba82
오선주 <oceanzoo@oseonjuui-MacBookPro.local>
2021-05-27 16:39:32 +0900

    Add: nav components 생성

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import "./Nav.scss";
+import './Nav.scss';
 
-class Nav extends React.Component{
-
+class Nav extends React.Component {
+  render() {
+    return (
+      <nav>
+        <div class="navWrapper">
+          <div className="mainLogo">
+            <span>westagram</span>
+          </div>
+          <div className="searchWrapper">
+            <input className="search" type="text" placeholder="검색" />
+            <i className="fas fa-search"></i>
+          </div>
+          <div className="iconWrapper">
+            <img alt="Home" src="/images/Home.svg" />
+            <img alt="share" src="/images/share.svg" />
+            <img alt="navigate" src="/images/nav.svg" />
+            <img alt="heart" src="/images/heart.svg" />
+            <img className="myProfile" alt="profile" src="/images/me.jpeg" />
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Nav;
