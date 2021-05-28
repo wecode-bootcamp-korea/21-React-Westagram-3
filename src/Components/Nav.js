744a174b30328ec6e88fac0214abbcf0b15b3e8c
오선주 <oceanzoo@oseonjuui-MacBookPro.local>
2021-05-28 16:06:30 +0900

    Add: 공통 component nav.js, nav.scss 추가

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -11,14 +11,20 @@ class Nav extends React.Component {
           </div>
           <div className="searchWrapper">
             <input className="search" type="text" placeholder="검색" />
-            <i className="fas fa-search"></i>
+            <span>
+              <i className="fas fa-search"></i>
+            </span>
           </div>
           <div className="iconWrapper">
-            <img alt="Home" src="/images/Home.svg" />
-            <img alt="share" src="/images/share.svg" />
-            <img alt="navigate" src="/images/nav.svg" />
-            <img alt="heart" src="/images/heart.svg" />
-            <img className="myProfile" alt="profile" src="/images/me.jpeg" />
+            <img alt="Home" src="/Images/nav/Home.svg" />
+            <img alt="share" src="/Images/nav/share.svg" />
+            <img alt="navigate" src="/Images/nav/nav.svg" />
+            <img alt="heart" src="/Images/nav/heart.svg" />
+            <img
+              className="myProfile"
+              alt="profile"
+              src="/Images/nav/me.jpeg"
+            />
           </div>
         </div>
       </nav>
